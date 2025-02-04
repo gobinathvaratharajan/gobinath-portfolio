7f4db0352efca2d1cd9ac58572b4b9e864c418a4
Gobinath.v <gobinathvb24@gmail.com>
2025-02-04 23:38:00 +0530

    initial commit

---
+++ components/providers.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { ThemeProvider } from 'next-themes';
+
+import React from 'react';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      enableSystem
+      attribute='class'
+      defaultTheme='system'
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
